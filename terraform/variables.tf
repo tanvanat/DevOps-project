1af08449467fa3bb588312fd3e12f67a30adbd2c
Front <tanvanat112@gmail.com>
2025-03-08 13:52:28 +0700

    second commit

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+    type = string
+    description = "The AWS region to use"
+    default = "us-east-1"
+}
+variable "public_key" {
+  type        = string
+  description = "Public key for SSH access"
+}
+variable "private_key" {
+  type        = string
+  description = "The private key for SSH access to the EC2 instance"
+  sensitive   = true # Mark as sensitive to prevent display in plan/apply output
+}
+
+
+
+variable "key_name" {
+  type        = string
+  description = "Name of the SSH key pair"
+}
+
